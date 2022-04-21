5616306e48aac99561473657edbb78d4cda17abe
Michael Adams <me@michdavidadams.com>
2022-04-21 18:02:00 -0400

    Added Woof to Portfolio

---
+++ _portfolio/woof.md
@@ -0,0 +1,25 @@
+---
+title: "Woof"
+excerpt: "Track workouts with your dog."
+permalink: /_portfolio/woof/
+header:
+  teaser: /assets/woof/WoofHeader.png
+gallery:
+  - url: /assets/woof/StartView.png
+    image_path: /assets/woof/StartView.png
+    alt: "Main screen"
+  - url: /assets/woof/WalkView.png
+    image_path: /assets/woof/WalkView.png
+    alt: "Walking workout"
+  - url: /assets/woof/PlayView.png
+    image_path: /assets/woof/PlayView.png
+    alt: "Play workout"
+---
+
+Woof is a workout tracker that allows you to track your walks and playtime with your dog.
+
+View your dog's progress towards their daily exercise goal and view their streak.
+
+During a walk, workouts will automatically be paused when it detects you've stopped moving for a bit.
+
+{% include gallery caption="Screenshots" %}
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -11,6 +11,12 @@ var store = [{
         "url": "/_portfolio/panuccispizza/",
         "teaser": "/assets/panuccispizza/pizza.png"
       },{
+        "title": "Woof",
+        "excerpt":"Woof is a workout tracker that allows you to track your walks and playtime with your dog.   View your dog’s progress towards their daily exercise goal and view their streak.   During a walk, workouts will automatically be paused when it detects you’ve stopped moving for a bit.                                                                                                                                Screenshots       ","categories": [],
+        "tags": [],
+        "url": "/_portfolio/woof/",
+        "teaser": "/assets/woof/WoofHeader.png"
+      },{
         "title": "Meddie",
         "excerpt":"My first time building an app was in 2019. I didn’t get very far, but it was fun building as much as I could before I simply needed to shelve it for later. I was building it using SwiftUI and I had only completed a single programming class (intro to...","categories": [],
         "tags": ["apps"],
